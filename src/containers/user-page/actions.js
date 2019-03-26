1364b82ad1e4b921a3be5085a49a1ee5834be611
Jitendra.Prajapati <Jitendra.Prajapati@3PAUS160>
2019-03-27 10:30:54 +1100

    Refactored User Actions to sepatate file.

--- src/containers/user-page/actions.js
+++ src/containers/user-page/actions.js
@@ -1,42 +1,27 @@
 import axios from 'axios'
 import HandleError from '../../utility/handle-error'
-
-export const FETCH_USERS = 'FETCH_USERS';
-export const FETCH_USERS_SUCCEED = 'FETCH_USERS_SUCCEED';
-export const FETCH_USERS_FAILED = 'FETCH_USERS_FAILED';
-
-export const ADD_USER = 'ADD_USER';
-export const SELECT_USER = 'SELECT_USER';
-
-export const CANCEL_USER = 'CANCEL_USER';
-export const SAVE_USER = 'SAVE_USER';
-export const SAVE_USER_SUCCEED = 'SAVE_USER_SUCCEED';
-export const SAVE_USER_FAILED = 'SAVE_USER_FAILED';
-
-export const DELETE_USER = 'DELETE_USER';
-export const DELETE_USER_SUCCEED = 'DELETE_USER_SUCCEED';
-export const DELETE_USER_FAILED = 'DELETE_USER_FAILED';
+import { USER_ACTIONS as U } from '../actionTypes'
 
 const API_URL = 'http://localhost:3333/api/users'
 
 export const fetchUsers = (criteria) => dispatch => {
-    dispatch({ type: FETCH_USERS })
+    dispatch({ type: U.FETCH_USERS })
 
     return axios.get(`${API_URL}/${criteria}`)   
     .then(({data}) => {
         dispatch({    
-            type: FETCH_USERS_SUCCEED,
+            type: U.FETCH_USERS_SUCCEED,
             payload: data
         })
     })
-    .catch(err => HandleError(FETCH_USERS_FAILED, err, dispatch))
+    .catch(err => HandleError(U.FETCH_USERS_FAILED, err, dispatch))
 }
 
 
 export const selectUser = (id) => {
     return dispatch => {
         dispatch({ 
-            type:SELECT_USER ,
+            type:U.SELECT_USER ,
             payload: id
         })
     }
@@ -45,7 +30,7 @@ export const selectUser = (id) => {
 export const addUser = () => {
     return dispatch => {
         dispatch({ 
-            type:ADD_USER
+            type:U.ADD_USER
         })
     }
 }
@@ -53,31 +38,31 @@ export const addUser = () => {
 export const cancelUser = () => {
     return dispatch => {
         dispatch({ 
-            type:CANCEL_USER
+            type:U.CANCEL_USER
         })
     }
 }
 
 export const saveUser = (user) => dispatch => {
-    dispatch({ type: SAVE_USER })
+    dispatch({ type: U.SAVE_USER })
 
     return axios.post(API_URL, user)   
     .then(({data}) => {
         dispatch({    
-            type: SAVE_USER_SUCCEED
+            type: U.SAVE_USER_SUCCEED
         })
     })
-    .catch(err => HandleError(SAVE_USER_FAILED, err, dispatch))
+    .catch(err => HandleError(U.SAVE_USER_FAILED, err, dispatch))
 }
 
 export const deleteUser = (id) => dispatch => {
-    dispatch({ type: DELETE_USER })
+    dispatch({ type: U.DELETE_USER })
 
     return axios.delete(`${API_URL}/${id}`)   
     .then(({data}) => {
         dispatch({    
-            type: DELETE_USER_SUCCEED
+            type: U.DELETE_USER_SUCCEED
         })
     })
-    .catch(err => HandleError(DELETE_USER_FAILED, err, dispatch))
+    .catch(err => HandleError(U.DELETE_USER_FAILED, err, dispatch))
 }
--- src/containers/user-page/reducer.js
+++ src/containers/user-page/reducer.js
@@ -1,8 +1,4 @@
-import { FETCH_USERS, FETCH_USERS_FAILED, FETCH_USERS_SUCCEED,
-            ADD_USER, SELECT_USER, CANCEL_USER, 
-            SAVE_USER, SAVE_USER_FAILED, SAVE_USER_SUCCEED,
-            DELETE_USER, DELETE_USER_FAILED, DELETE_USER_SUCCEED
-         } from './actions'
+import { USER_ACTIONS as U } from '../actionTypes'
 
 const initUser = { 
     id:0,
@@ -19,51 +15,51 @@ const initialState = {
 const users = (state=initialState, action) => {
 
     switch (action.type) {
-        case FETCH_USERS:
+        case U.FETCH_USERS:
             return {...state, fetching:true , error:null}
         
-        case FETCH_USERS_SUCCEED: {
+        case U.FETCH_USERS_SUCCEED: {
             return {...state, fetching:false, users:action.payload }
         }
 
-        case FETCH_USERS_FAILED: {
+        case U.FETCH_USERS_FAILED: {
             return {...state, fetching:false }
         }
 
-        case ADD_USER: {
+        case U.ADD_USER: {
             return {...state, user: initUser }
         }
 
-        case SELECT_USER:{
+        case U.SELECT_USER:{
             const user = state.users.find(u => u.id === action.payload)
             return {...state, user:user }
         }
 
-        case CANCEL_USER:{
+        case U.CANCEL_USER:{
             return {...state, user:null }
         }
 
-        case SAVE_USER: {
+        case U.SAVE_USER: {
             return { ...state, saving: true }
         }
 
-        case SAVE_USER_SUCCEED: {
+        case U.SAVE_USER_SUCCEED: {
             return { ...state, saving: false }
         }
 
-        case SAVE_USER_FAILED: {
+        case U.SAVE_USER_FAILED: {
             return { ...state, saving: false }
         }
 
-        case DELETE_USER: {
+        case U.DELETE_USER: {
             return {...state, deleting:true }
         }
 
-        case DELETE_USER_SUCCEED: {
+        case U.DELETE_USER_SUCCEED: {
             return { ...state, user:null, deleting: false }
         }
 
-        case DELETE_USER_FAILED: {
+        case U.DELETE_USER_FAILED: {
             return { ...state, deleting: false }
         }        
 
