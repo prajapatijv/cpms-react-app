a54d4b1faf58fa2c54796f836fccbaa8236f5b0d
Jitendra.Prajapati <Jitendra.Prajapati@3PAUS160>
2019-04-08 13:37:30 +1000

    Style updates for status component.

--- src/components/shared/status-bar.js
+++ src/components/shared/status-bar.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { stat } from 'fs';
+import classNames from 'classnames';
+
 
 const StatusBar = ({status,  clearAll}) => {
     const hasErrors = (status.errors && status.errors.length > 0) 
     const hasInfos = (status.infos && status.infos.length > 0) 
+
+    var cls = classNames({
+        'alert alert-dismissible fade show app-alert': true,
+        'alert-danger': hasErrors,
+        'alert-info': hasInfos
+    })
+
     return (
     (hasInfos || hasErrors) &&
-     <div className="alert alert-danger alert-dismissible fade show app-alert" role="alert">
-        <strong className="alert-heading">{hasErrors ? "Error" : "Info"}  </strong>
-            {status.errors.join(" ")}
+     <div className={cls} role="alert">
+        <strong className="alert-heading">
+            {hasErrors ? status.errors.join(" ") : status.infos.join(" ")}  
+        </strong>
         <button type="button" className="close" data-dismiss="alert" aria-label="Close" 
             onClick={() => clearAll()}>
             <span aria-hidden="true">&times;</span>
