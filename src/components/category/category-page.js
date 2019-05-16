5ec0c1546d08adec11009d443b87c1eb175d0adf
Jitendra.Prajapati <Jitendra.Prajapati@3PAUS160>
2019-05-16 13:03:29 +1000

    Start intgrating material table.

--- src/components/category/category-page.js
+++ src/components/category/category-page.js
@@ -3,24 +3,34 @@ import React from 'react'
 import CategoryForm from './category-form'
 import NavLink from '../shared/navlink'
 import PageWrapper from '../shared/page-wrapper'
+import { TableHead, TableRow, TableCell, TableBody } from '@material-ui/core'
 
 const CategoryPage = (props) => {
     return (
         <PageWrapper searchLabel="categories" {...props}>
-            <CategoryLines {...props}/>
+            <CategoryLines {...props} />
             <CategoryForm {...props} />
         </PageWrapper>
     )
 }
 
-const CategoryLines = ({ categories }) => 
-    categories.map((category) => 
-        <NavLink key={category.id} 
-            to={`/categories/${category.id}`}
-            activeClassName="active"
-            className="app-list-item list-group-item-action"
-            >{category.name} {category.shortName}
-        </NavLink>
-    )
+const CategoryLines = ({ categories }) =>
+<React.Fragment>
+    <TableHead>
+        <TableRow>
+            <TableCell align="left">ID</TableCell>
+            <TableCell align="left">Name</TableCell>
+            <TableCell align="left">ShortName</TableCell>
+        </TableRow>
+    </TableHead>
+    <TableBody>
+        {categories.map(category => (
+            <TableRow key={category.id}>
+                {category.name}
+            </TableRow>
+            ), this, 
+        )}
+    </TableBody>
+    </React.Fragment>
 
 export default CategoryPage
