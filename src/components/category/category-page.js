a2692a0bcbb7c232262fa8b2e75f58118756a54b
Jitendra.Prajapati <Jitendra.Prajapati@3PAUS160>
2019-05-16 16:41:37 +1000

    Markup changes.

--- src/components/category/category-page.js
+++ src/components/category/category-page.js
@@ -7,20 +7,37 @@ import PageWrapper from '../shared/page-wrapper'
 const CategoryPage = (props) => {
     return (
         <PageWrapper searchLabel="categories" {...props}>
-            <CategoryLines {...props}/>
+            <CategoryLines {...props} />
             <CategoryForm {...props} />
         </PageWrapper>
     )
 }
 
-const CategoryLines = ({ categories }) => 
-    categories.map((category) => 
-        <NavLink key={category.id} 
+const CategoryLines = ({ categories }) =>
+    categories.map((category) =>
+        <NavLink key={category.id}
             to={`/categories/${category.id}`}
             activeClassName="active"
             className="app-list-item list-group-item-action"
-            >{category.name} {category.shortName}
+        >{category.name} {category.shortName}
         </NavLink>
     )
+/*<table className="table table-hover">
+    <thead>
+        <tr>
+            <td>Name</td>
+            <td>Short Name</td>
+        </tr>
+    </thead>
+    <tbody>
+        {categories.map((category) =>  
+        <tr>
+            <td>{category.name}</td>
+            <td>{category.shortName}</td>
+        </tr>
+        )}
+    </tbody>
+</table>*/
+
 
 export default CategoryPage
